e76f40e49146ae5fc5e97763fa21975c83353b0e
25LP4270 25LP4270 <25LP4270@pinecones-iMac.local>
2025-06-18 17:10:23 +0800

    hi

--- backend/src/controllers/food.controller.ts
+++ backend/src/controllers/food.controller.ts
@@ -1,19 +1,47 @@
-export const getAllFoods = (request, response) => {
-  response.send("food-category/ Get huselt irlee");
+import { Request, Response } from "express";
+import { Food } from "../models/index.js";
+
+export const getAllFoods = async (request: Request, response: Response) => {
+  try {
+    const foods = await Food.find();
+
+    response.json({
+      success: true,
+      data: foods,
+    });
+  } catch (error) {
+    response.status(444).json({
+      success: false,
+      error: error,
+    });
+  }
 };
 
-export const getFoodByid = (request, response) => {
+export const getFoodByid = (request: Request, response: Response) => {
   response.send("food/:foodId Get huselt irlee");
 };
 
-export const createFood = (request, response) => {
-  response.send("food/ Post huselt irlee");
+export const createFood = async (request: Request, response: Response) => {
+  try {
+    const food = request.body;
+    const createdFood = await Food.create(food);
+
+    response.json({
+      success: true,
+      data: createdFood,
+    });
+  } catch (error) {
+    response.status(444).json({
+      success: false,
+      error: error,
+    });
+  }
 };
 
-export const updateFood = (request, response) => {
+export const updateFood = (request: Request, response: Response) => {
   response.send("food/:foodId Patch huselt irlee");
 };
 
-export const deleteFood = (request, response) => {
+export const deleteFood = (request: Request, response: Response) => {
   response.send("food/:foodId Delete huselt irlee");
 };
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,11 @@
 import express from "express";
 import booksRouter from "./routes/book.routes.js";
 import foodsRouter from "./routes/food.routes.js";
+import mongoose from "mongoose";
+
+mongoose.connect(
+  "mongodb+srv://batb9714:VfzylQqTaDhUziWq@cluster0.xtttvsm.mongodb.net/food-delivery"
+);
 
 const server = express();
 server.use(express.json());
@@ -8,7 +13,7 @@ server.use(express.json());
 const port = 3333;
 
 server.use("/book", booksRouter);
-server.use("/food", foodsRouter);
+server.use("/buuz", foodsRouter);
 
 server.get("/", (_request, response) => {
   response.send("Hello Dashka");
